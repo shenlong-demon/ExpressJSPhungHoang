28fd33bd52ff5a52bfa4f3113842fb14a16cf839
VNDANLAP-0051\LONG <shenlongdemon@gmail.com>
2024-08-16 10:13:35 +0700

    #tag version 1.0.0

--- src/business/repositories/TestRepo.ts
+++ src/business/repositories/TestRepo.ts
@@ -4,7 +4,7 @@ import { Logger } from '@core/common';
 export class TestRepo {
 	static async get(): Promise<any> {
 		const userCount = await prisma.phuser.count();
-		const data = { userCount, metadata: 1, migration: '0004_add_receiptAt_to_bill_table.sql' };
+		const data = { userCount, metadata: 1, migration: '0004_add_receiptAt_to_bill_table.sql', hash: '***9851b1', version: '1.0.0' };
 		Logger.log(() => [`TestRepo get`, data]);
 		return data;
 	}
